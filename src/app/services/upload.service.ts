1457f89fa2827c681183f62b5560f497d8df6a7b
Perparim Koci <rimakosrimi93@gmail.com>
2021-05-16 09:36:04 +0200

    service upload

--- src/app/services/upload.service.ts
+++ src/app/services/upload.service.ts
@@ -10,17 +10,14 @@ export class UploadService {
 
   constructor(private http: HttpClient) {
   }
-
   upload(file: File): Observable<HttpEvent<any>> {
     const formData: FormData = new FormData();
 
     formData.append('file', file);
-
     const req = new HttpRequest('POST', `${environment.apiUrl}/files`, formData, {
       reportProgress: true,
       responseType: 'json'
     });
-
     return this.http.request(req);
   }
 }
